6d44641824a527875ac5d99d9d462ee784257821
Domask <domask2@mail.ru>
2020-09-11 10:01:12 +0600

    feat:solved 1 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,3 +4,28 @@ module.exports = function countCats(/* matrix */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
+
+module.exports = function countCats(matrix) {
+  
+	 let sum = 0;
+
+	for(let i = 0; i<matrix.length; i++) {
+		for(let j = 0; j<matrix[i].length; j++){
+
+			if(matrix[i][j] == '^^') {
+				sum++
+			}
+		}
+	}
+
+	return sum;
+
+
+  //  return matrix.reduce( (sum, arr) => 
+  //   		sum + arr.reduce( (total, item) => 
+  //     (total + (item == "^^" ? 1 : 0) ), 
+  //   0), 
+  // 0);
+
+};
