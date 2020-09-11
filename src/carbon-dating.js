2eaa5eba3ee768f70e70be68651013bb4fe6a90b
Domask <domask2@mail.ru>
2020-09-11 10:04:20 +0600

    feat:solved 2 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,3 +7,16 @@ module.exports = function dateSample(/* sampleActivity */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
+module.exports = function dateSample(sample) {
+ 
+	if(typeof sample !== "string" || sample<=0 || sample > MODERN_ACTIVITY ||
+		/[a-zA-Z]/.test(sample)) return false
+
+	let key = 0.693 / HALF_LIFE_PERIOD;
+
+	let res = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sample)) / key)
+
+	return res
+
+};
