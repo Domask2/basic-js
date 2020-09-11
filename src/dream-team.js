f1180764a45f1622b52f5477f2265373a8af4fac
Domask <domask2@mail.ru>
2020-09-11 14:13:15 +0600

    feat:solved 3 task

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,3 +4,17 @@ module.exports = function createDreamTeam(/* members */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
+module.exports = function createDreamTeam(members) {
+ if(!Array.isArray(members)) return false;
+  
+  let res = [];
+  members.forEach((item,i)=> {
+    if( typeof item === 'string') {
+      res.push(item.replace(/\s+/g, '').split('')[0].toUpperCase())
+    } return false
+  })
+
+  return res.sort().join('')
+};
+
